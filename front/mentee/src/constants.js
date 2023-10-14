34d950ad924526d48830a31d61a72b572dce2926
shamil-b <shamilbedru47@gmail.com>
2023-10-14 21:44:39 +0300

    deployment configuration

--- front/mentee/src/constants.js
+++ front/mentee/src/constants.js
@@ -1,6 +1,7 @@
 const globalIp = "http://192.168.135.214:5000";
 // const localIp = "https://mentee-deployment-new-backend.vercel.app";
-const localIp = "http://localhost:5000";
+// const localIp = "http://localhost:5000";
+const localIp = "https://mentee-final-backend-e5gh9xl23-shamil-b.vercel.app/";
 const baseApi = localIp;
 const baseFrontApi = "http://localhost:3000" ;
 const localLinks = [
